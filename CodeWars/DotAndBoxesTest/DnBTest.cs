c5623a3c8bcd6bee5b07df51b1157a6626240ac2
san-had <hadhazis@gmail.com>
2019-11-28 21:45:31 +0100

    added default test

--- CodeWars/DotAndBoxesTest/DnBTest.cs
+++ CodeWars/DotAndBoxesTest/DnBTest.cs
@@ -1,3 +1,4 @@
+using DotAndBoxes;
 using NUnit.Framework;
 
 namespace DotAndBoxesTest
@@ -5,14 +6,31 @@ namespace DotAndBoxesTest
     public class DnBTest
     {
         [SetUp]
-        public void Setup()
+        public void TestInitialize()
         {
         }
 
         [Test]
-        public void Test1()
+        public void DnBBaseTest()
         {
-            Assert.Pass();
+            var moves = new int[][]
+             {
+                new[] { 0, 1 },
+                new[] { 7, 8 },
+                new[] { 1, 2 },
+                new[] { 6, 7 },
+                new[] { 0, 3 },
+                new[] { 8, 5 },
+                new[] { 3, 4 },
+                new[] { 4, 1 },
+                new[] { 4, 5 },
+                new[] { 2, 5 },
+                new[] { 7, 4 },
+                new[] { 3, 6 }
+             };
+            var expected = new int[2] { 3, 1 };
+            var actual = DnB.DotsAndBoxes(moves);
+            Assert.AreEqual(expected, actual);
         }
     }
 }
