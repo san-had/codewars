dda1ff7d0e358c4c6b1fe27cbcf6136dfd677016
shadhazi <sandor.hadhazi@qualysoft.com>
2019-11-29 16:29:40 +0100

    Added Squares

--- CodeWars/DotAndBoxes/DnB.cs
+++ CodeWars/DotAndBoxes/DnB.cs
@@ -1,11 +1,41 @@
-﻿namespace DotAndBoxes
+﻿using System;
+using System.Collections.Generic;
+
+namespace DotAndBoxes
 {
     public class DnB
     {
         public static int[] DotsAndBoxes(int[][] moves)
         {
             var result = new int[2] { 3, 1 };
+
+            var length = 3;
+
+            var square1 = CreateSquare(length);
+
             return result;
         }
+
+        private static List<Square> CreateSquares(int length)
+        {
+        }
+
+        private static Square CreateSquare(int length)
+        {
+            var side1 = Tuple.Create(0, 1);
+            var side2 = Tuple.Create(1, 4);
+            var side3 = Tuple.Create(3, 4);
+            var side4 = Tuple.Create(0, 3);
+
+            var square = new Square()
+            { Sides = { side1, side2, side3, side4 } };
+
+            return square;
+        }
+    }
+
+    internal class Square
+    {
+        public List<Tuple<int, int>> Sides { get; set; }
     }
 }
