aae834043a53a37a84f983edf8f62f9ce3288d97
shadhazi <sandor.hadhazi@qualysoft.com>
2019-12-17 08:45:30 +0100

    added equalto24

---
+++ CodeWars/GameEqual24/GE24.cs
@@ -0,0 +1,10 @@
+﻿namespace GameEqual24
+{
+    public class GE24
+    {
+        public static string EqualTo24(int num1, int num2, int num3, int num4)
+        {
+            return "It's not possible!";
+        }
+    }
+}
