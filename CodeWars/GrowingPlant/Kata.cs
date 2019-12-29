542f536d1c2d06ddb9ff19c637c8d80c995a30ae
san-had <hadhazis@gmail.com>
2019-12-29 20:23:52 +0100

    resolved growing plant

---
+++ CodeWars/GrowingPlant/Kata.cs
@@ -0,0 +1,23 @@
+﻿namespace GrowingPlant
+{
+    public class Kata
+    {
+        public static int GrowingPlant(int upSpeed, int downSpeed, int desiredHeight)
+        {
+            int days = 0;
+            int currentHeight = 0;
+
+            do
+            {
+                if (days != 0)
+                {
+                    currentHeight -= downSpeed;
+                }
+                days++;
+                currentHeight += upSpeed;
+            } while (currentHeight < desiredHeight);
+
+            return days;
+        }
+    }
+}
---
+++ CodeWars/GrowingPlant/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GrowingPlant
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.GrowingPlant(100, 10, 910));
+        }
+    }
+}
