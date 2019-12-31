95000da0ed5ec315c7b71fdac5e5993e57e7715e
san-had <hadhazis@gmail.com>
2019-12-31 15:54:49 +0100

    resolved blackjack in CSharp

--- CodeWars/BlackJack/Kata.cs
+++ CodeWars/BlackJack/Kata.cs
@@ -1,12 +1,54 @@
-﻿namespace BlackJack
+﻿using System.Collections.Generic;
+
+namespace BlackJack
 {
     public class Kata
     {
+        private static readonly Dictionary<string, int> cardValuesA1 = SetupCardValues(1);
+
+        private static readonly Dictionary<string, int> cardValuesA11 = SetupCardValues(11);
+
         public static int ScoreHand(string[] cards)
         {
-            int value = 0;
+            int valueWithA1 = 0;
 
-            return value;
+            int valueWithA11 = 0;
+
+            foreach (var card in cards)
+            {
+                valueWithA1 += cardValuesA1[card];
+                valueWithA11 += cardValuesA11[card];
+            }
+
+            int below21 = 21 - valueWithA1;
+
+            int above21 = valueWithA11 - 21;
+
+            if (below21 <= above21)
+            {
+                return valueWithA1;
+            }
+            else
+            {
+                return valueWithA11;
+            }
+        }
+
+        private static Dictionary<string, int> SetupCardValues(int aValue)
+        {
+            var cardValues = new Dictionary<string, int>();
+
+            for (int i = 2; i < 11; i++)
+            {
+                cardValues.Add(i.ToString(), i);
+            }
+
+            cardValues.Add("J", 10);
+            cardValues.Add("Q", 10);
+            cardValues.Add("K", 10);
+            cardValues.Add("A", aValue);
+
+            return cardValues;
         }
     }
 }
--- CodeWars/BlackJack/Program.cs
+++ CodeWars/BlackJack/Program.cs
@@ -4,11 +4,22 @@ namespace BlackJack
 {
     internal class Program
     {
-        private static void Main(string[] args)
+        private static void Main()
         {
-            string[] cards = { "1", "2", "J", "A" };
-
+            string[] cards = { "A" };
             Console.WriteLine(Kata.ScoreHand(cards));
+
+            string[] cards2 = { "A", "J" };
+            Console.WriteLine(Kata.ScoreHand(cards2));
+
+            string[] cards3 = { "A", "10", "A" };
+            Console.WriteLine(Kata.ScoreHand(cards3));
+
+            string[] cards4 = { "5", "3", "7" };
+            Console.WriteLine(Kata.ScoreHand(cards4));
+
+            string[] cards5 = { "5", "4", "3", "2", "A", "K" };
+            Console.WriteLine(Kata.ScoreHand(cards5));
         }
     }
 }
