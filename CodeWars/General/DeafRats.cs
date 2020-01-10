f10c6314522af6a3e014838bd306d1914c598402
shadhazi <sandor.hadhazi@qualysoft.com>
2020-01-10 14:57:34 +0100

    not yet final

--- CodeWars/General/DeafRats.cs
+++ CodeWars/General/DeafRats.cs
@@ -1,4 +1,6 @@
-﻿namespace General
+﻿using System.Linq;
+
+namespace General
 {
     public class DeafRats
     {
@@ -6,11 +8,61 @@
         {
             int deafRatsCount = 0;
 
-            string sequenceAll = town.Replace(" ", string.Empty)
-                                  .Replace("~O", "L")
-                                  .Replace("O~", "R");
+            string sequenceAll = GetSequence(town);
+
+            string[] sequences = sequenceAll.Split('P');
+
+            char direction = 'R';
+
+            foreach (var sequence in sequences)
+            {
+                if (direction == ' ')
+                {
+                    direction = GetDirection(sequence);
+                }
+                deafRatsCount += GetDeafRatsCount(direction, sequence);
+
+                direction = direction == 'L' ? 'R' : 'L';
+            }
 
             return deafRatsCount;
+        }
+
+        private static int GetDeafRatsCount(char direction, string sequence)
+        {
+            return sequence.Count(ch => ch != direction);
+        }
+
+        private static char GetDirection(string sequence)
+        {
+            int leftDirection = sequence.Count(x => x == 'L');
+            int rightDirection = sequence.Count(x => x == 'R');
+
+            return leftDirection >= rightDirection ? 'L' : 'R';
+        }
+
+        private static string GetSequence(string town)
+        {
+            string filteredTown1 = town.Replace("0", "O");
+
+            int firstIndexOfWave = filteredTown1.IndexOf('~');
+            int firstIndexOfO = filteredTown1.IndexOf('O');
+
+            string filteredTown2 = string.Empty;
+            if (firstIndexOfO > firstIndexOfWave)
+            {
+                filteredTown2 = filteredTown1.Replace("~O", "R")
+                                             .Replace("O~", "L");
+            }
+            else
+            {
+                filteredTown2 = filteredTown1.Replace("O~", "L")
+                                              .Replace("~O", "R");
+            }
+
+            string sequenceAll = filteredTown2.Replace(" ", string.Empty);
+
+            return sequenceAll;
         }
     }
 }
