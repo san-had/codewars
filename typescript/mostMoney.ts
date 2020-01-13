7f24f8526bddd15aff620b601b04fd950cea6589
shadhazi <sandor.hadhazi@qualysoft.com>
2020-01-13 11:57:01 +0100

    MostMoney in progress

--- typescript/mostMoney.ts
+++ typescript/mostMoney.ts
@@ -1,5 +1,21 @@
 import { Student } from "./Student";
 
 export function mostMoney(students: Student[]) {
+
+    let reducedStudentArray: reducedStudent[] = students.map(student => 
+        {
+            const money: number = student.fives * 5 + student.tens * 10 + student.twenties * 20;
+            return new reducedStudent(student.name, money);
+        });
+
+    let numberOfMaxMoney : number = reducedStudentArray
+        .filter(student => student.money == Math.max(student.money)).length;
     
+
+}
+
+class reducedStudent {
+    constructor(public name: string, public money: number ) {
+
+    }
 }
