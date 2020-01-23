4da4c1a6ac348fbb9ddfd7c6510f6a717026a13c
san-had <hadhazis@gmail.com>
2020-01-23 21:46:59 +0100

    begin with sports league

---
+++ CodeWars/SportsLeague/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace SportsLeague
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int[,] games = new int[,]
+            {
+                {0, 5, 2, 2 },
+                {1, 4, 0, 2 },
+                {2, 3, 1, 2 },
+                {1, 5, 2, 2 },
+                {2, 0, 1, 1 },
+                {3, 4, 1, 1 },
+                {2, 5, 0, 2 },
+                {3, 1, 1, 1 },
+                {4, 0, 2, 0 }
+            };
+
+            //var games =  [[0, 5, 2, 2]];   // Team 0 - Team 5 => 2:2
+            //            //[1, 4, 0, 2],   // Team 1 - Team 4 => 0:2
+            //            //[2, 3, 1, 2],   // Team 2 - Team 3 => 1:2
+            //            //[1, 5, 2, 2],   // Team 1 - Team 5 => 2:2
+            //            //[2, 0, 1, 1],   // Team 2 - Team 0 => 1:1
+            //            //[3, 4, 1, 1],   // Team 3 - Team 4 => 1:1
+            //            //[2, 5, 0, 2],   // Team 2 - Team 5 => 0:2
+            //            //[3, 1, 1, 1],   // Team 3 - Team 1 => 1:1
+            //            //[4, 0, 2, 0]]   // Team 4 - Team 0 => 2:0
+        }
+    }
+}
