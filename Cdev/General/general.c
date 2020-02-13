9f74d88eaac8b5fa763c9258840312713afa1458
shadhazi <sandor.hadhazi@qualysoft.com>
2020-02-13 09:40:53 +0100

    completing takeFirstN

--- Cdev/General/general.c
+++ Cdev/General/general.c
@@ -7,46 +7,37 @@ int main(void)
 {
 	puts("Hello world!");
 
-	unsigned short num = 10;
+	unsigned short num = 50;
 
-	//unsigned short reverse_array[10];
 	unsigned short* reverse_array = reverse_seq(num);
-
-	//unsigned short first_item = *reverse_array;
-
-	//unsigned short second_item = *(reverse_array + 1);
-
-	//unsigned short third_item = *(reverse_array + 2);
-
-	//printf("%u\n", first_item);
-
-	//printf("%u\n", first_item);
-
-	//printf("%u\n", second_item);
-
-	//printf("%u\n", third_item);
-
-	//unsigned short item = *reverse_array;
 
 	for (unsigned short i = 0; i < num; i++)
 	{
 		unsigned short item = *(reverse_array + i);
-		printf("%u\n", item);
+		printf("%d\n", item);
 	}
-
-	//for (size_t i = 0; i < sizeof(reverse_array); i++)
-	//{
-	//}
 }
 
 unsigned short* reverse_seq(unsigned short num)
 {
-	unsigned short reverse_array[10];
-
-	for (size_t i = 0; i < num; i++)
+	if (num == 0)
 	{
-		reverse_array[i] = num;
-		num--;
+		return NULL;
+	}
+
+	unsigned short length = num;
+
+	static unsigned short* reverse_array;
+	reverse_array = (unsigned short*)malloc(sizeof(unsigned short) * num);
+
+	for (size_t i = 0; i < length; i++)
+	{
+		if (reverse_array != NULL)
+		{
+			//reverse_array[i] = num;
+			*(reverse_array + i) = num;
+			num--;
+		}
 	}
 	return reverse_array;
 }
