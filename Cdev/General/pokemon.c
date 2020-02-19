5436369242eec4bcb7b75d67bcc90b4701bf25a8
san-had <hadhazis@gmail.com>
2020-02-19 20:46:53 +0100

    pokemon C completed

--- Cdev/General/pokemon.c
+++ Cdev/General/pokemon.c
@@ -16,17 +16,20 @@ char* get_key(const char* your_type, const char* opponent_type);
 
 int main(void)
 {
-	printf("%d\n", calculate_damage("fire", "water", 100, 100));
-	printf("%d\n", calculate_damage("grass", "water", 100, 100));
-	printf("%d\n", calculate_damage("electric", "fire", 100, 100));
-	printf("%d\n", calculate_damage("grass", "electric", 57, 19));
-	printf("%d\n", calculate_damage("grass", "water", 40, 40));
-	printf("%d\n", calculate_damage("grass", "fire", 35, 5));
-	printf("%d\n", calculate_damage("fire", "electric", 10, 2));
+	//printf("%d\n", calculate_damage("fire", "water", 100, 100));
+	//printf("%d\n", calculate_damage("grass", "water", 100, 100));
+	//printf("%d\n", calculate_damage("electric", "fire", 100, 100));
+	//printf("%d\n", calculate_damage("grass", "electric", 57, 19));
+	//printf("%d\n", calculate_damage("grass", "water", 40, 40));
+	//printf("%d\n", calculate_damage("grass", "fire", 35, 5));
+	//printf("%d\n", calculate_damage("fire", "electric", 10, 2));
 
-	printf("%d\n", calculate_damage("fire", "water", 100, 100));
-	printf("%d\n", calculate_damage("electric", "fire", 24, 1));
-	printf("%d\n", calculate_damage("electric", "grass", 84, 1));
+	//printf("%d\n", calculate_damage("fire", "water", 100, 100));
+	//printf("%d\n", calculate_damage("electric", "fire", 24, 1));
+	//printf("%d\n", calculate_damage("electric", "grass", 84, 1));
+
+	printf("%d\n", calculate_damage("water", "water", 100, 100));
+	printf("%d\n", calculate_damage("grass", "grass", 75, 1));
 }
 
 int calculate_damage(const char* your_type, const char* opponent_type, int attack, int defense)
@@ -37,6 +40,11 @@ int calculate_damage(const char* your_type, const char* opponent_type, int attac
 
 double get_effectiveness(const char* your_type, const char* opponent_type)
 {
+	if (!strncmp(your_type, opponent_type, 20))
+	{
+		return 0.5;
+	}
+
 	double e_value = 1;
 
 	char* key = get_key(your_type, opponent_type);
@@ -49,7 +57,7 @@ double get_effectiveness(const char* your_type, const char* opponent_type)
 	{
 		e = *(effectivenesses + i);
 
-		int eq = strncmp(e.key, key);
+		int eq = strncmp(e.key, key, 20);
 		if (!eq)
 		{
 			e_value = e.value;
@@ -113,6 +121,10 @@ effectiveness* load_effectiveness()
 		{"grass-fire", 0.5},
 		{"fire-water", 0.5},
 		{"water-grass", 0.5}
+		//{"electric-electric", 0.5},
+		//{"fire-fire", 0.5},
+		//{"grass-grass", 0.5},
+		//{"water-water", 0.5}
 	};
 
 	return effectiveness_array;
