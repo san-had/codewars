f1bba7d030638921e7d7bfee3bb82dd020a6ba83
san-had <hadhazis@gmail.com>
2020-03-08 21:09:21 +0100

    Cpmpleting linked list

--- Cdev/HelloC/linked_list_learn.c
+++ Cdev/HelloC/linked_list_learn.c
@@ -12,6 +12,14 @@ void push(node_t* head, int val);
 
 void insert(node_t** head, int val);
 
+int pop_first(node_t** head);
+
+int remove_last(node_t* head);
+
+int remove_by_index(node_t** head, int n);
+
+int remove_by_value(node_t** head, int val);
+
 int main(void) {
 	node_t* head = NULL;
 
@@ -37,7 +45,20 @@ int main(void) {
 
 	insert(&head, 0);
 
+	insert(&head, 15);
+
 	print_list(head);
+
+	pop_first(&head);
+
+	print_list(head);
+
+	//int retval = remove_by_index(head, 2);
+	int retval = remove_by_value(head, 3);
+
+	print_list(head);
+
+	printf("%d\n", retval);
 
 	return EXIT_SUCCESS;
 }
@@ -81,5 +102,85 @@ void insert(node_t** head, int val)
 		new_node->val = val;
 		new_node->next = *head;
 		*head = new_node;
+	}
+}
+
+int pop_first(node_t** head)
+{
+	int retVal = -1;
+
+	node_t* next_node = NULL;
+
+	if (*head == NULL)
+	{
+		return -1;
+	}
+
+	next_node = (*head)->next;
+	retVal = (*head)->val;
+	free(*head);
+	*head = next_node;
+}
+
+int remove_last(node_t* head)
+{
+	node_t* current = head;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+	}
+}
+
+int remove_by_index(node_t** head, int n)
+{
+	int index = 0;
+	int retval = -1;
+	node_t* current = head;
+	node_t* temp_node = NULL;
+
+	if (n == 0)
+	{
+		return pop_first(n);
+	}
+
+	for (index = 0; index < n - 1; index++)
+	{
+		if (current->next == NULL)
+		{
+			return -1;
+		}
+		current = current->next;
+	}
+
+	temp_node = current->next;
+	retval = temp_node->val;
+	current->next = temp_node->next;
+	free(temp_node);
+
+	return retval;
+}
+
+int remove_by_value(node_t** head, int val)
+{
+	node_t* current = head;
+	node_t* temp_node = NULL;
+
+	int retval = -1;
+
+	temp_node = current;
+	while (current->next != NULL)
+	{
+		if (current->val == val)
+		{
+			temp_node->next = current->next;
+			retval = current->val;
+			break;
+		}
+		else
+		{
+			temp_node = current;
+			current = current->next;
+		}
 	}
 }
