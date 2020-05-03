c53d7732e0e01b43fdb0a88fd474a65604105bb2
san-had <hadhazis@gmail.com>
2020-05-03 15:22:00 +0200

    NumberZooPatrol own solution

---
+++ CodeWars/NumberZooPatrol/Kata.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace NumberZooPatrol
+{
+    public class Kata
+    {
+        public static int FindNumber(int[] animals)
+        {
+            List<int> animalsList = new List<int>();
+            animalsList.AddRange(animals);
+            animalsList.Sort();
+
+            int missing = GetMissing(animalsList);
+
+            return missing;
+        }
+
+        private static int GetMissing(List<int> animalsList)
+        {
+            int missing = animalsList.Count + 1;
+            for (int index = 0; index < animalsList.Count; index++)
+            {
+                if (index + 1 != animalsList[index])
+                {
+                    missing = index + 1;
+                    break;
+                }
+            }
+
+            return missing;
+        }
+    }
+}
