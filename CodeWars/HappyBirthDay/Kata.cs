a67415d8a7f1581c3ec3bacbd9f643033672902d
shadhazi <sandor.hadhazi@qualysoft.com>
2020-05-08 15:16:53 +0200

    HappyBirthday plus review

---
+++ CodeWars/HappyBirthDay/Kata.cs
@@ -0,0 +1,16 @@
+﻿namespace HappyBirthDay
+{
+    public class Kata
+    {
+        public static string WomensAge(int age)
+        {
+            int inBase = age / 2;
+            int calcAge = 20 + age % 2;
+
+            return $"{age}? That's just {calcAge}, in base {inBase}!";
+        }
+
+        public static string WomensAge2(int age) =>
+            $"{age}? That's just {20 + age % 2}, in base {age / 2}!";
+    }
+}
---
+++ CodeWars/HappyBirthDay/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HappyBirthDay
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.WomensAge2(39));
+
+            Console.WriteLine(Kata.WomensAge2(32));
+        }
+    }
+}
