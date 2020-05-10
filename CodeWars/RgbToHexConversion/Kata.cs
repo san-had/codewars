2226960752f9398aae64cd40fa8c71d7ef4ef994
san-had <hadhazis@gmail.com>
2020-05-10 14:58:04 +0200

    Added Rgb conversion

---
+++ CodeWars/RgbToHexConversion/Kata.cs
@@ -0,0 +1,20 @@
+﻿namespace RgbToHexConversion
+{
+    public class Kata
+    {
+        public static string Rgb(int r, int g, int b)
+        {
+            return HexConverter(r) + HexConverter(g) + HexConverter(b);
+        }
+
+        private static string HexConverter(int input)
+        {
+            if (input < 0)
+            {
+                input = 0;
+            }
+
+            return input > 255 ? "FF" : input.ToString("X2");
+        }
+    }
+}
---
+++ CodeWars/RgbToHexConversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RgbToHexConversion
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.Rgb(148, -20, 211));
+        }
+    }
+}
