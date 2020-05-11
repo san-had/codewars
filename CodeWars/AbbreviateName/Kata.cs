afbba17f013b0af4c3937a7f2424874f5d0d1428
shadhazi <sandor.hadhazi@qualysoft.com>
2020-05-11 09:07:02 +0200

    Abbrev Name + review

---
+++ CodeWars/AbbreviateName/Kata.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace AbbreviateName
+{
+    public class Kata
+    {
+        public static string AbbrevName(string name)
+        {
+            var nameArray = name.ToUpper().Split(' ');
+
+            return $"{nameArray[0][0]}.{nameArray[1][0]}";
+        }
+
+        public static string AbbrevName2(string name)
+        {
+            return string.Join(".", name.ToUpper().Split(' ').Select(w => w[0]));
+        }
+    }
+}
---
+++ CodeWars/AbbreviateName/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AbbreviateName
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine(Kata.AbbrevName2("Sam Harris"));
+        }
+    }
+}
