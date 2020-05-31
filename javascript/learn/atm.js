28647343f7d781ebe3d6c7dafae3a0a6017387b6
san-had <hadhazis@gmail.com>
2020-05-31 15:27:11 +0200

    clarifyied version

--- javascript/learn/atm.js
+++ javascript/learn/atm.js
@@ -1,23 +1,18 @@
 function solve(n) {
-    var rest = n;
+    if (n % 10 !== 0) {
+        return -1;
+    }
+
+
     var number_of_notes = 0;
     var notes = [500,200,100,50,20,10];
     for (var i = 0; i < notes.length; i++) {
-        console.log(i + ": " + notes[i]);
-        while (rest - notes[i] >= 0) {
-            rest -= notes[i];
+        while (n - notes[i] >= 0) {
+            n -= notes[i];
             number_of_notes += 1;
-            console.log("-" + notes[i]);
-            console.log(rest);
-            console.log(number_of_notes);            
         }    
     }
-    if (rest === 0) {
-        return number_of_notes;
-    }
-    else {
-        return -1;
-    }
+    return number_of_notes;
 }
 
-console.log(solve(42));
+console.log(solve(1250));
